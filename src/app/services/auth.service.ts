d5babca12237ce2edaa730ba6ac648d364076826
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2021-10-24 19:02:40 +0300

    Create register component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,4 +24,13 @@ export class AuthService {
   logOut() {
     this.afAuth.signOut();
   }
+
+  register(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.createUserWithEmailAndPassword(email, password).then(
+        (userData) => resolve(userData),
+        (err) => reject(err)
+      );
+    });
+  }
 }
