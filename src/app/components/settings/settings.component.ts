6dcd02f31636dbe28c75e86e8497942c3f42be9b
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2021-10-24 20:02:15 +0300

    Complete settings component, save settings in local storage

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Settings } from '../../models/Settings';
+import { SettingsService } from '../../services/settings.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private settingsService: SettingsService,
+    private flashMsg: FlashMessagesService
+  ) {
+    this.settings = this.settingsService.getSettings();
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.settingsService.changeSettings(this.settings);
+    this.flashMsg.show('Settings saved', {
+      cssClass: 'alert-success',
+      timeout: 4000,
+    });
+  }
 }
